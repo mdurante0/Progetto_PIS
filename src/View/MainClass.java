cc62c97d46c9e141cebb576d3eee68a59ff9af7b
mdurante <mattiadurante0099@gmail.com>
2022-09-05 19:43:17 +0200

    applicazione avviata

--- src/View/MainClass.java
+++ src/View/MainClass.java
@@ -10,6 +10,7 @@ public class MainClass {
 
     public static void main(String args[]) {
         System.out.println("applicazione avviata");
+
         /*
         JFrame finestra = new JFrame("Prima finestra");
 
