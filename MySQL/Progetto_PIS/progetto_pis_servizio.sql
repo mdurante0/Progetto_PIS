2e601986f576bdf6ed8e49838703db8b7eab414c
mdurante0 <105500053+mdurante0@users.noreply.github.com>
2022-09-05 13:59:09 +0200

    Add files via upload

---
+++ MySQL/Progetto_PIS/progetto_pis_servizio.sql
@@ -0,0 +1,54 @@
+-- MySQL dump 10.13  Distrib 8.0.29, for Win64 (x86_64)
+--
+-- Host: localhost    Database: progetto_pis
+-- ------------------------------------------------------
+-- Server version	8.0.29
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `servizio`
+--
+
+DROP TABLE IF EXISTS `servizio`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `servizio` (
+  `articolo_idarticolo` int NOT NULL,
+  `fornitore_idfornitore` int NOT NULL,
+  PRIMARY KEY (`articolo_idarticolo`),
+  KEY `fk_servizio_fornitore1_idx` (`fornitore_idfornitore`),
+  KEY `fk_servizio_articolo1_idx` (`articolo_idarticolo`),
+  CONSTRAINT `fk_servizio_articolo1` FOREIGN KEY (`articolo_idarticolo`) REFERENCES `articolo` (`idarticolo`),
+  CONSTRAINT `fk_servizio_fornitore1` FOREIGN KEY (`fornitore_idfornitore`) REFERENCES `fornitore` (`idfornitore`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `servizio`
+--
+
+LOCK TABLES `servizio` WRITE;
+/*!40000 ALTER TABLE `servizio` DISABLE KEYS */;
+/*!40000 ALTER TABLE `servizio` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-09-05 13:21:34
