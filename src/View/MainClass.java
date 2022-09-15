dae81ffc2330d0b6005e2c87497e791f331de715
mdurante0 <mattiadurante0099@gmail.com>
2022-09-15 16:51:49 +0200

    id -> idUtente

--- src/View/MainClass.java
+++ src/View/MainClass.java
@@ -8,7 +8,7 @@ import Model.composite.Prodotto;
 
 public class MainClass {
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
 
         /*
         JFrame finestra = new JFrame("Prima finestra");
