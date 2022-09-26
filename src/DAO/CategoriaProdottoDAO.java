0a7735b32bb1f3f57b904fd146ff5ab92c6923c3
ant9501 <antonio.scelsi@studenti.unisalento.it>
2022-09-26 17:58:50 +0200

    Tua zia quella pelata in carrozzella e col cerchietto

---
+++ src/DAO/CategoriaProdottoDAO.java
@@ -0,0 +1,129 @@
+package DAO;
+
+import DbInterface.IDbConnection;
+import DbInterface.command.DbOperationExecutor;
+import DbInterface.command.IDbOperation;
+import DbInterface.command.ReadOperation;
+import DbInterface.command.WriteOperation;
+import Model.Amministratore;
+import Model.CategoriaProdotto;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CategoriaProdottoDAO implements ICategoriaProdottoDAO {
+    private static CategoriaProdottoDAO instance = new CategoriaProdottoDAO();
+    private CategoriaProdotto categoria;
+    private static IDbConnection conn;
+    private static ResultSet rs;
+
+    private CategoriaProdottoDAO() {
+        categoria = null;
+        conn = null;
+        rs = null;
+    }
+
+    public static CategoriaProdottoDAO getInstance() {
+        return instance;
+    }
+
+    @Override
+    public CategoriaProdotto findById(String name) {
+
+        DbOperationExecutor executor = new DbOperationExecutor();
+        String sql = "SELECT idcategoria, nome, categoria_idcategoria FROM progetto_pis.categoria " +
+                "WHERE nome = '"+name+"';";
+        IDbOperation readOp = new ReadOperation(sql);
+        rs = executor.executeOperation(readOp).getResultSet();
+
+        try {
+            rs.next();
+            if (rs.getRow()==1) {
+                categoria = new CategoriaProdotto();
+                categoria.setNome(rs.getString("nome"));
+
+                return categoria;
+            }
+        } catch (SQLException e) {
+            // handle any errors
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        } catch (NullPointerException e) {
+            // handle any errors
+            System.out.println("Resultset: " + e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<CategoriaProdotto> findAll() {
+        DbOperationExecutor executor = new DbOperationExecutor();
+        String sql = "SELECT nome, categoria_idcategoria FROM progetto_pis.categoria ;";
+        IDbOperation readOp = new ReadOperation(sql);
+        rs = executor.executeOperation(readOp).getResultSet();
+
+        ArrayList<CategoriaProdotto> categorie = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                categoria = new CategoriaProdotto();
+                categoria.setNome(rs.getString("nome"));
+
+                categorie.add(categoria);
+            }
+            return categorie;
+        } catch (SQLException e) {
+            // Gestisce le differenti categorie d'errore
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        } catch (NullPointerException e) {
+            // Gestisce le differenti categorie d'errore
+            System.out.println("Resultset: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    @Override
+    public int add(CategoriaProdotto categoria) {
+/*
+        DbOperationExecutor executor = new DbOperationExecutor();
+        String sql = "INSERT INTO progetto_pis.amministratore (utente_idutente) VALUES (LAST_INSERT_ID());";
+        IDbOperation writeOp = new WriteOperation(sql);
+        return executor.executeOperation(writeOp).getRowsAffected();
+ */
+        DbOperationExecutor executor = new DbOperationExecutor();
+        String sql = "INSERT INTO progetto_pis.categoria (nome) VALUES ('"+categoria.getNome()+"');";
+        IDbOperation writeOp = new WriteOperation(sql);
+
+
+        return  executor.executeOperation(writeOp).getRowsAffected();
+
+
+    }
+
+    @Override
+    public int removeById(String name) {
+        String sql = "DELETE FROM progetto_pis.categoria " +
+                "WHERE nome = '"+ name + "';";
+
+        DbOperationExecutor executor = new DbOperationExecutor();
+        IDbOperation writeOp = new WriteOperation(sql);
+        return executor.executeOperation(writeOp).getRowsAffected();
+    }
+
+    @Override
+    public int update(CategoriaProdotto categoria) {
+
+        String sql = "UPDATE progetto_pis.categoria " +
+                "SET nome = '" + categoria.getNome() +
+                //"', categoria_idcategoria = '" + categoria.getIdCategoriaProdotto() +
+                "' WHERE nome = '" + categoria.getNome() + "';";
+
+        DbOperationExecutor executor = new DbOperationExecutor();
+        IDbOperation writeOp = new WriteOperation(sql);
+        return executor.executeOperation(writeOp).getRowsAffected();
+    }
+}
---
+++ src/DAO/CategoriaServizioDAO.java
@@ -0,0 +1,129 @@
+package DAO;
+
+import DbInterface.IDbConnection;
+import DbInterface.command.DbOperationExecutor;
+import DbInterface.command.IDbOperation;
+import DbInterface.command.ReadOperation;
+import DbInterface.command.WriteOperation;
+import Model.CategoriaProdotto;
+import Model.CategoriaServizio;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+public class CategoriaServizioDAO implements ICategoriaServizioDAO {
+    private static CategoriaServizioDAO instance = new CategoriaServizioDAO();
+    private CategoriaServizio categoria;
+    private static IDbConnection conn;
+    private static ResultSet rs;
+
+    private CategoriaServizioDAO() {
+        categoria = null;
+        conn = null;
+        rs = null;
+    }
+
+    public static CategoriaServizioDAO getInstance() {
+        return instance;
+    }
+
+    @Override
+    public CategoriaServizio findById(String name) {
+
+        DbOperationExecutor executor = new DbOperationExecutor();
+        String sql = "SELECT idcategoria, nome FROM progetto_pis.categoria " +
+                "WHERE nome = '"+name+"';";
+        IDbOperation readOp = new ReadOperation(sql);
+        rs = executor.executeOperation(readOp).getResultSet();
+
+        try {
+            rs.next();
+            if (rs.getRow()==1) {
+                categoria = new CategoriaServizio();
+                categoria.setNome(rs.getString("nome"));
+
+                return categoria;
+            }
+        } catch (SQLException e) {
+            // handle any errors
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        } catch (NullPointerException e) {
+            // handle any errors
+            System.out.println("Resultset: " + e.getMessage());
+        }
+        return null;
+    }
+
+    @Override
+    public ArrayList<CategoriaServizio> findAll() {
+        DbOperationExecutor executor = new DbOperationExecutor();
+        String sql = "SELECT nome FROM progetto_pis.categoria ;";
+        IDbOperation readOp = new ReadOperation(sql);
+        rs = executor.executeOperation(readOp).getResultSet();
+
+        ArrayList<CategoriaServizio> categorie = new ArrayList<>();
+        try {
+            while (rs.next()) {
+                categoria = new CategoriaServizio();
+                categoria.setNome(rs.getString("nome"));
+
+                categorie.add(categoria);
+            }
+            return categorie;
+        } catch (SQLException e) {
+            // Gestisce le differenti categorie d'errore
+            System.out.println("SQLException: " + e.getMessage());
+            System.out.println("SQLState: " + e.getSQLState());
+            System.out.println("VendorError: " + e.getErrorCode());
+        } catch (NullPointerException e) {
+            // Gestisce le differenti categorie d'errore
+            System.out.println("Resultset: " + e.getMessage());
+        }
+
+        return null;
+    }
+
+    @Override
+    public int add(CategoriaServizio categoria) {
+/*
+        DbOperationExecutor executor = new DbOperationExecutor();
+        String sql = "INSERT INTO progetto_pis.amministratore (utente_idutente) VALUES (LAST_INSERT_ID());";
+        IDbOperation writeOp = new WriteOperation(sql);
+        return executor.executeOperation(writeOp).getRowsAffected();
+ */
+        DbOperationExecutor executor = new DbOperationExecutor();
+        String sql = "INSERT INTO progetto_pis.categoria (nome) VALUES ('"+categoria.getNome()+"');";
+        IDbOperation writeOp = new WriteOperation(sql);
+
+
+        return  executor.executeOperation(writeOp).getRowsAffected();
+
+
+    }
+
+    @Override
+    public int removeById(String name) {
+        String sql = "DELETE FROM progetto_pis.categoria " +
+                "WHERE nome = '"+ name + "';";
+
+        DbOperationExecutor executor = new DbOperationExecutor();
+        IDbOperation writeOp = new WriteOperation(sql);
+        return executor.executeOperation(writeOp).getRowsAffected();
+    }
+
+    @Override
+    public int update(CategoriaServizio categoria) {
+
+        String sql = "UPDATE progetto_pis.categoria " +
+                "SET nome = '" + categoria.getNome() +
+                //"', categoria_idcategoria = '" + categoria.getIdCategoriaProdotto() +
+                "' WHERE nome = '" + categoria.getNome() + "';";
+
+        DbOperationExecutor executor = new DbOperationExecutor();
+        IDbOperation writeOp = new WriteOperation(sql);
+        return executor.executeOperation(writeOp).getRowsAffected();
+    }
+}
