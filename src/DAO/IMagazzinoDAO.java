bc9aedb3ae474457e771ca195bafacb32470276c
ant9501 <antonio.scelsi@studenti.unisalento.it>
2023-03-07 16:16:33 +0100

    vada

--- src/DAO/IMagazzinoDAO.java
+++ src/DAO/IMagazzinoDAO.java
@@ -2,13 +2,16 @@ package DAO;
 
 import Model.CategoriaProdotto;
 import Model.Magazzino;
+import Model.composite.IProdotto;
 
 import java.util.ArrayList;
 
 public interface IMagazzinoDAO {
-    Magazzino findById(int id);
+    Magazzino findById(String indirizzo);
     ArrayList<Magazzino> findAll();
     int add(Magazzino magazzino);
     int removeById(int id);
     int update(Magazzino magazzino);
+    int addProdotto(int idMagazzino, IProdotto iProdotto);
+    int removeProdotto(int idMagazzino, IProdotto iProdotto);
 }
