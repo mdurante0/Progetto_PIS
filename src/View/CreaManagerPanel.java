4e3100f64eb65e02841623c6552852fe8f96a1a0
Ant95-01 <134865013+Ant95-01@users.noreply.github.com>
2023-06-15 11:50:57 +0200

    Aggiungere action listener su tutti questi pannelli di merda

---
+++ src/View/CreaManagerPanel.java
@@ -0,0 +1,99 @@
+package View;
+
+import Business.PuntoVenditaBusiness;
+import Business.Results.PuntoVenditaResult;
+import Model.PuntoVendita;
+import View.Listener.GoToLoginListener;
+import View.Listener.RegistrationListener;
+
+import javax.swing.*;
+import java.awt.*;
+import java.util.Iterator;
+
+public class CreaManagerPanel extends JPanel {
+    private MainFrame frame;
+    private JPanel titlePanel = new JPanel();
+    private JPanel contentPanel = new JPanel();
+    private JTextField firstNameField;
+    private JTextField lastNameField;
+    private JTextField emailField;
+    private JTextField usernameField;
+    private JPasswordField passwordField;
+    private JPasswordField confermaPasswordField;
+    private JTextField durataIncaricoField;
+
+
+    public CreaManagerPanel(MainFrame frame) {
+        this.frame = frame;
+
+        this.setLayout(new BorderLayout());
+        JLabel titleLabel = new JLabel("Crea Manager");
+        Font titleFont = new Font(Font.SANS_SERIF, Font.BOLD, 30);
+        titleLabel.setFont(titleFont);
+        titlePanel.add(titleLabel);
+
+        contentPanel.setLayout(new GridLayout(15,2));
+        JLabel firstNameLabel = new JLabel("  Nome:");
+        JLabel lastNameLabel = new JLabel("  Cognome:");
+        JLabel emailLabel = new JLabel("  Email:");
+        JLabel usernameLabel = new JLabel("  Username:");
+        JLabel passwordLabel = new JLabel("  Password:");
+        JLabel confermaPasswordLabel = new JLabel("  Conferma password:");
+        JLabel durataincaricoLabel = new JLabel("  Durata incarico:");
+
+
+        Font bodyFont = new Font(Font.DIALOG, Font.ITALIC, 20);
+        firstNameLabel.setFont(bodyFont);
+        lastNameLabel.setFont(bodyFont);
+        emailLabel.setFont(bodyFont);
+        usernameLabel.setFont(bodyFont);
+        passwordLabel.setFont(bodyFont);
+        confermaPasswordLabel.setFont(bodyFont);
+        durataincaricoLabel.setFont(bodyFont);
+
+
+        firstNameField = new JTextField(20);
+        lastNameField = new JTextField(20);
+        emailField = new JTextField(20);
+        usernameField = new JTextField(20);
+        passwordField = new JPasswordField(20);
+        confermaPasswordField = new JPasswordField(20);
+        durataIncaricoField = new JTextField(20);
+
+        firstNameField.setFont(bodyFont);
+        lastNameField.setFont(bodyFont);
+        emailField.setFont(bodyFont);
+        usernameField.setFont(bodyFont);
+        passwordField.setFont(bodyFont);
+        confermaPasswordField.setFont(bodyFont);
+        durataIncaricoField.setFont(bodyFont);
+
+
+        JButton registerButton = new JButton("Aggiungi");
+        JButton backButton = new JButton("Indietro");
+
+        // ggiungere action listener
+
+        contentPanel.add(firstNameLabel);
+        contentPanel.add(firstNameField);
+        contentPanel.add(lastNameLabel);
+        contentPanel.add(lastNameField);
+        contentPanel.add(emailLabel);
+        contentPanel.add(emailField);
+        contentPanel.add(usernameLabel);
+        contentPanel.add(usernameField);
+        contentPanel.add(passwordLabel);
+        contentPanel.add(passwordField);
+        contentPanel.add(confermaPasswordLabel);
+        contentPanel.add(confermaPasswordField);
+        contentPanel.add(durataincaricoLabel);
+        contentPanel.add(durataIncaricoField);
+        contentPanel.add(new JLabel());
+        contentPanel.add(new JLabel());
+        contentPanel.add(backButton);
+        contentPanel.add(registerButton);
+
+        this.add(titlePanel, BorderLayout.PAGE_START);
+        this.add(contentPanel, BorderLayout.CENTER);
+    }
+}
---
+++ src/View/ModificaProduttorePanel.java
@@ -0,0 +1,101 @@
+package View;
+
+import Model.Produttore;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class ModificaProduttorePanel extends  JPanel{
+    private MainFrame frame;
+    private JPanel titlePanel = new JPanel();
+    private JPanel contentPanel = new JPanel();
+
+
+    public ModificaProduttorePanel(MainFrame frame, Produttore produttore) {
+        this.frame = frame;
+
+        this.setLayout(new BorderLayout());
+
+        JLabel titleLabel = new JLabel("Fornitore");
+        Font titleFont = new Font(Font.SANS_SERIF, Font.BOLD, 30);
+        titleLabel.setFont(titleFont);
+        titlePanel.add(titleLabel);
+        JTextField nomeField;
+        JTextField emailField;
+        JTextField telefonoField;
+        JTextField nazioneField;
+        JTextField cittaField;
+        JTextField descrizioneField;
+        JTextField sitoField;
+
+
+        contentPanel.setLayout(new GridLayout(15,2));
+        JLabel firstNameLabel = new JLabel("  Nome:");
+        JLabel emailLabel = new JLabel("  Email:");
+        JLabel telefonoLabel = new JLabel("  Telefono");
+        JLabel nazioneLabel = new JLabel("  Nazione:");
+        JLabel cittaLabel = new JLabel("  Città:");
+        JLabel descrizioneLabel = new JLabel("  Descrizione:");
+        JLabel sitoLabel = new JLabel("  Sito:");
+
+        Font bodyFont = new Font(Font.DIALOG, Font.ITALIC, 20);
+        firstNameLabel.setFont(bodyFont);
+        emailLabel.setFont(bodyFont);
+        telefonoLabel.setFont(bodyFont);
+        nazioneLabel.setFont(bodyFont);
+        cittaLabel.setFont(bodyFont);
+        descrizioneLabel.setFont(bodyFont);
+        sitoLabel.setFont(bodyFont);
+
+
+
+        nomeField = new JTextField(produttore.getNome(),20);
+
+        emailField = new JTextField(produttore.getMail(),20);
+        telefonoField = new JTextField(produttore.getTelefono(),20);
+        nazioneField = new JTextField(produttore.getNazione(),20);
+        cittaField = new JTextField(produttore.getCitta(),20);
+        descrizioneField = new JTextField(produttore.getDescrizione(),20);
+        sitoField = new JTextField(produttore.getSito(),20);
+
+
+        nomeField.setFont(bodyFont);
+        emailField.setFont(bodyFont);
+        telefonoField.setFont(bodyFont);
+        nazioneField.setFont(bodyFont);
+        cittaField.setFont(bodyFont);
+        descrizioneField.setFont(bodyFont);
+        sitoField.setFont(bodyFont);
+
+
+        JButton modificaFornitoreButton = new JButton("Modifica");
+        modificaFornitoreButton.setFont(bodyFont);
+        JButton tornaIndietroButton = new JButton("Torna indietro");
+        tornaIndietroButton.setFont(bodyFont);
+
+        // aggiungere gli action listener
+
+        contentPanel.add(firstNameLabel);
+        contentPanel.add(nomeField);
+        contentPanel.add(emailLabel);
+        contentPanel.add(emailField);
+        contentPanel.add(telefonoLabel);
+        contentPanel.add(telefonoField);
+        contentPanel.add(nazioneLabel);
+        contentPanel.add(nazioneField);
+        contentPanel.add(cittaLabel);
+        contentPanel.add(cittaField);
+        contentPanel.add(descrizioneLabel);
+        contentPanel.add(descrizioneField);
+        contentPanel.add(sitoLabel);
+        contentPanel.add(sitoField);
+        contentPanel.add(new JLabel());
+        contentPanel.add(new JLabel());
+        contentPanel.add(tornaIndietroButton);
+        contentPanel.add(modificaFornitoreButton);
+
+        this.add(titlePanel, BorderLayout.PAGE_START);
+        this.add(contentPanel, BorderLayout.CENTER);
+    }
+
+}
