2016a36d53fcb0ccee43a587e79cd4d8d42bb42d
Ant95-01 <134865013+Ant95-01@users.noreply.github.com>
2023-06-16 16:58:48 +0200

    Aggiungere action listener

---
+++ src/View/CreaRispostaFeedbackProdottoPanel.java
@@ -0,0 +1,63 @@
+package View;
+
+
+import Model.Feedback;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class CreaRispostaFeedbackProdottoPanel extends JPanel {
+    private MainFrame frame;
+    private JPanel titlePanel = new JPanel();
+    private JPanel contentPanel = new JPanel();
+
+    private JTextArea rispostaField;
+
+
+    public CreaRispostaFeedbackProdottoPanel(MainFrame frame) {
+        this.frame = frame;
+
+        this.setLayout(new BorderLayout());
+
+        JLabel titleLabel = new JLabel("Nuova Risposta");
+        Font titleFont = new Font(Font.SANS_SERIF, Font.BOLD, 30);
+        titleLabel.setFont(titleFont);
+        titlePanel.add(titleLabel);
+
+        contentPanel.setLayout(new GridLayout(4,2));
+        JLabel categoriaLabel = new JLabel("  Risposta:");
+
+
+        Font bodyFont = new Font(Font.DIALOG, Font.ITALIC, 15);
+        categoriaLabel.setFont(bodyFont);
+
+
+        rispostaField = new JTextArea();
+
+
+
+        rispostaField.setFont(bodyFont);
+
+
+        JButton aggiungiRispostaButton = new JButton("Aggiungi");
+        aggiungiRispostaButton.setFont(bodyFont);
+        JButton tornaIndietroButton = new JButton("Torna indietro");
+        tornaIndietroButton.setFont(bodyFont);
+
+        // aggiungere gli action listener
+
+        contentPanel.add(categoriaLabel);
+        contentPanel.add(rispostaField);
+
+        contentPanel.add(new JLabel());
+        contentPanel.add(new JLabel());
+        contentPanel.add(tornaIndietroButton);
+        contentPanel.add(aggiungiRispostaButton);
+
+        this.add(titlePanel, BorderLayout.PAGE_START);
+        this.add(contentPanel, BorderLayout.CENTER);
+    }
+
+
+
+}
